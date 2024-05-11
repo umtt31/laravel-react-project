b0451e6f8ad9a67abc52556db6f84bd7c6ca51eb
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-05-12 00:52:15 +0300

    implement signup, login & logout in API

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'signup']);
