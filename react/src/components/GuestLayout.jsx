2c65fee0cd8510c440629e25b0dfb38d4f6cd737
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-05-11 23:08:20 +0300

    Protecting routes

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
+    const { token } = useStateContext();
+
+    if (token) {
+        return <Navigate to={"/users"} />;
+    }
+
     return (
         <div>
-            <div>
-                <Outlet />
-            </div>
+            <Outlet />
         </div>
     );
 }
